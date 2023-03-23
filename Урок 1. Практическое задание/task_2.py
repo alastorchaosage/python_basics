4d70aafdfd926acfaf136542b3c4d94b441de27d
alastorchaosage <128675142+alastorchaosage@users.noreply.github.com>
2023-03-23 15:34:35 +0300

    task_2.py

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,15 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+
+sec_into = int(input('Введите время в секундах: '))
+sec = sec_into % (24 * 3600)
+hou_res = str(sec // 3600)
+min_res = str(sec % 3600 // 60)
+sec_res = str(sec_into % 60)
+time_res = (hou_res + ':' + min_res + ':' + sec_res)
+print(f"Время в формате чч:мм:сс - ", time_res.format('%HH:%MM:%SS'))
+
+
+
+
